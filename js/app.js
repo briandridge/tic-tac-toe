2a8c117413300f8ad2f394cd1ab4f071c1786cb5
briandridge <briandridge@gmail.com>
2016-11-09 20:22:57 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+
+
+var clickBox1 = function() {
+	var xArray = [];
+	var oArray = [];
+	
+
+	var box1 = document.getElementById("1");	
+	var click = document.addEventListener("click", function() {
+	box1.innerHTML = "x";
+	xArray.push(box1);
+	if xArray.length === odd sum;
+	then
+
+
+	var clickNumber = 0;
+	var anyBox = document.getElementById("1");
+	console.log(anyBox);
+	document.anyBox.onclick = function(){
+    clickNumber  ++ ;
+    console.log(clickNumber);
+  	};  
+
+})};
+
+
+
+clickBox1();
+
+var xArray = [];
+
+
+
+
+
+// check for winning combos
+// if xArray === [1,2,3] || [4,5,6] || [7,8,9] || [1,4,7] || [2,5,8] || [3,6,9] || [1,5,9] || [3,5,7] {
+// 		alert("Success")
+// 	}
